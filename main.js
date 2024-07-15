3d67f34d8d52b6c88999f3ef292a84b062770c9c
laralord33 <laralord33@gmail.com>
2024-07-15 15:39:21 +0000

    newl

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+// Add JS here
